16b46bbf7a6ebd56dd4d34cff5221d978bb7f52d
suhailshamsuddin <suhailshams666@gmail.com>
2019-03-22 17:28:10 +0530

    Update multi.c

--- multi.c
+++ multi.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int i,a=29,product;
+int i,number,product;
+printf("enter number:");
+scanf("%d",&number);
 for(i=1;i<11;i++)
 	{
-		product=i*a;
-		printf("%d*%d=%d\n",a,i,product);
+		product=i*number;
+		printf("%d*%d=%d\n",number,i,product);
 	}
 }
