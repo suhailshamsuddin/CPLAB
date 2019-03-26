d23b618545997aec7cfb107a4d8afbc3d61070d1
suhailshamsuddin <suhailshams666@gmail.com>
2019-03-26 16:51:04 +0530

    Add files via upload

--- bin.c
+++ bin.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 void main()
 {
-	int rem,bin=0,num,i=1;
-	printf("Enter the number: ");
-	scanf("%d",&num);
-	while(num>0)
-		{
+int rem,bin=0,num,i=1;
+printf("enter the number");
+scanf("%d",&num);
+while(num>0)
+	{
 		rem=num%2;
-		bin=bin+rem*i;
 		num=num/2;
+		bin=bin+(rem*i);
 		i=i*10;
 		}
-	
-	printf("The bin equivalent is:%d",bin);
+printf("binary equalent=%d",bin);
 }
